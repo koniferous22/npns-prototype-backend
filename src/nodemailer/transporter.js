40f0f5c1601d4087908c97943ef41c3b917e68b2
koniferous22 <dekkel22@gmail.com>
2019-11-30 21:16:08 +0100

    Reworked emails, cuz prod not working lol

--- src/nodemailer/transporter.js
+++ src/nodemailer/transporter.js
@@ -1,13 +1,21 @@
 const nodemailer = require('nodemailer')
 
-const transporter = nodemailer.createTransport({
-    host: process.env.NODEMAILER_HOST,
-    port: process.env.NODEMAILER_PORT,
-    secure: false,
-    auth: {
-        user: process.env.NODEMAILER_USER,
-        pass: process.env.NODEMAILER_PASSWORD
-    }
-});
+const transporter = (process.env.NODEMAILER_MODE === 'production') ? 
+		nodemailer.createTransport({
+			service: 'SendGrid',
+			auth: {
+				user: process.env.NODEMAILER_USER,
+				pass: 
+			}
+		})
+	: 	nodemailer.createTransport({
+		    host: process.env.NODEMAILER_HOST,
+		    port: process.env.NODEMAILER_PORT,
+		    secure: false,
+		    auth: {
+		        user: process.env.NODEMAILER_USER,
+		        pass: process.env.NODEMAILER_PASSWORD
+		    }
+		});
 
 module.exports = transporter
